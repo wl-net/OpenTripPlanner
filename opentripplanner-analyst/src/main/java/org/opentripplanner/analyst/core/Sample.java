d9fc9d0b23672a3ca25c5158c4f9a031b748b04c
Andrew Byrd <andrew@fastmail.net>
2012-10-29 19:15:34 +0100

    redefine Sample as a simple data holder class

--- opentripplanner-analyst/src/main/java/org/opentripplanner/analyst/core/Sample.java
+++ opentripplanner-analyst/src/main/java/org/opentripplanner/analyst/core/Sample.java
@@ -1,53 +1,28 @@
 package org.opentripplanner.analyst.core;
 
-import org.opentripplanner.routing.core.State;
 import org.opentripplanner.routing.graph.Vertex;
-import org.opentripplanner.routing.spt.ShortestPathTree;
 
+/**
+ * A Sample holds some cached data about the relationship between a geographic point and a graph.
+ * It is the result of some relatively time-consuming distance operations and can be cached to speed
+ * up subsequent re-evaluations of the travel time surface with respect to a different SPT.
+ * It contains references to two vertices and the distances to those vertices because 
+ * we assume that each point may be accessible from two different vertices, which are usually
+ * the two ends of the nearest street.
+ */
 public class Sample {
-    public final int t0, t1;
+    public final float d0, d1;
     public final Vertex v0, v1;
     
-    public Sample (Vertex v0, int t0, Vertex v1, int t1) {
+    public Sample (Vertex v0, float d0, Vertex v1, float d1) {
         this.v0 = v0;
-        this.t0 = t0;
+        this.d0 = d0;
         this.v1 = v1;
-        this.t1 = t1;
-    }
-    
-    public byte evalBoardings(ShortestPathTree spt) {
-        State s0 = spt.getState(v0);
-        State s1 = spt.getState(v1);
-        int m0 = 255;
-        int m1 = 255;
-        if (s0 != null)
-            m0 = (s0.getNumBoardings()); 
-        if (s1 != null)
-            m1 = (s1.getNumBoardings()); 
-        return (byte) ((m0 < m1) ? m0 : m1); 
-    }
-    
-    public byte evalByte(ShortestPathTree spt) {
-        long t = eval(spt) / 60;
-        if (t >= 255)
-            t = 255;
-        return (byte) t;
-    }
-    
-    public long eval(ShortestPathTree spt) {
-        State s0 = spt.getState(v0);
-        State s1 = spt.getState(v1);
-        long m0 = Long.MAX_VALUE;
-        long m1 = Long.MAX_VALUE;
-        if (s0 != null)
-            m0 = (s0.getActiveTime() + t0); 
-        if (s1 != null)
-            m1 = (s1.getActiveTime() + t1); 
-        return (m0 < m1) ? m0 : m1; 
+        this.d1 = d1;
     }
     
     public String toString() {
-        return String.format("Sample: %s in %d sec or %s in %d sec\n", v0, t0, v1, t1);
+        return String.format("Sample: %s at %d m or %s at %d m\n", v0, d0, v1, d1);
     }
     
 }
