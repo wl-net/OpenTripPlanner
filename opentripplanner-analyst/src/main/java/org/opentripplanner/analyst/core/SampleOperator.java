7ccaae87808bb8a475e77f070a322bfe9ef53b23
Andrew Byrd <andrew@fastmail.net>
2012-10-29 19:16:39 +0100

    shared superclass methods in SampleOperator

--- opentripplanner-analyst/src/main/java/org/opentripplanner/analyst/core/SampleOperator.java
+++ opentripplanner-analyst/src/main/java/org/opentripplanner/analyst/core/SampleOperator.java
@@ -31,37 +31,71 @@ public abstract class SampleOperator {
     /** Implement this method to supply logic for converting States to result numbers */
     public abstract int evaluate(State state, double distance);
 
-    public ResultSet evaluate(ShortestPathTree spt, Population population) {
-        
-        
-        double[] results = new double[population.totalSize()];
-        
-        // replace Sample with Sample[] or Pair<Sample>?
-        int i = 0;
-        for (Individual indiv : population) { // iterate over samples that have not been filtered out
-            int extreme = minimize ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-            int bestResult = extreme;
-            // if skip[i]...
-            Sample s = indiv.sample;
-            // add null s0s1 checks
-            State s0 = spt.getState(s.v0);
-            State s1 = spt.getState(s.v1);
-            if (s0 != null)
-                bestResult = evaluate(s0, s.t0);
-            if (s1 != null) {
-                int r = evaluate(s1, s.t1);
-                if (minimize) {
-                    if (r < bestResult)
-                        bestResult = r;
-                } else {
-                    if (r > bestResult)
-                        bestResult = r;
-                }
+    /**
+     * Find the States in the ShortestPathTree for the given Sample, evaluate each Sample, and
+     * return the better of the two results if there is more than one.
+     */
+    private float evaluate(ShortestPathTree spt, Sample sample) {
+        float bestResult = minimize ? Float.POSITIVE_INFINITY : Float.NEGATIVE_INFINITY;
+        State s0 = spt.getState(sample.v0);
+        State s1 = spt.getState(sample.v1);
+        if (s0 != null)
+            bestResult = evaluate(s0, sample.d0);
+        if (s1 != null) {
+            int r = evaluate(s1, sample.d1);
+            if (minimize) {
+                if (r < bestResult)
+                    bestResult = r;
+            } else {
+                if (r > bestResult)
+                    bestResult = r;
             }
-            results[i] = (bestResult == extreme) ? Integer.MAX_VALUE : bestResult;
         }
-        // maybe change result sets to use ints instead of doubles? do we need FP values?
+        return bestResult;
+    }
+
+    public ResultSet evaluate(ShortestPathTree spt, Population population) {        
+        
+        float[] results = new float[population.totalSize()];
+        int i = 0;
+        for (Individual indiv : population) { // iterate over samples that have not been filtered out            
+            Sample sample = indiv.sample;
+            results[i] = evaluate(spt, sample);
+        }
         return new ResultSet(population, spt, results);
     }
     
+    // TODO implement these obsolete methods in subclasses
+    
+//    public byte evalBoardings(ShortestPathTree spt) {
+//        State s0 = spt.getState(v0);
+//        State s1 = spt.getState(v1);
+//        int m0 = 255;
+//        int m1 = 255;
+//        if (s0 != null)
+//            m0 = (s0.getNumBoardings()); 
+//        if (s1 != null)
+//            m1 = (s1.getNumBoardings()); 
+//        return (byte) ((m0 < m1) ? m0 : m1); 
+//    }
+//    
+//    public byte evalByte(ShortestPathTree spt) {
+//        long t = eval(spt) / 60;
+//        if (t >= 255)
+//            t = 255;
+//        return (byte) t;
+//    }
+//    
+//    public long eval(ShortestPathTree spt) {
+//        State s0 = spt.getState(v0);
+//        State s1 = spt.getState(v1);
+//        long m0 = Long.MAX_VALUE;
+//        long m1 = Long.MAX_VALUE;
+//        if (s0 != null)
+//            m0 = (s0.getActiveTime() + t0); 
+//        if (s1 != null)
+//            m1 = (s1.getActiveTime() + t1); 
+//        return (m0 < m1) ? m0 : m1; 
+//    }
+
 }
